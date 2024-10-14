dec1710deaf6896d7e23bcd0339382618ab14d0d
unknown <aleksandravicius.vakaris07@gmail.com>
2024-10-14 16:13:49 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static String makeOutWord(String out, String word) {
+        return out.substring(0, 2) + word + out.substring(2);
+
+
+    }
+    public static void main(String[] args) {
+        System.out.println(makeOutWord("<<>>", "Yay"));
+        System.out.println(makeOutWord("<<>>", "WooHoo"));
+        System.out.println(makeOutWord("[[]]", "word"));
+    }
+}
